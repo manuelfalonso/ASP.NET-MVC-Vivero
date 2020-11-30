63cefb6675b85db8b9ca1052c4a49009eea0d1e0
Jonathan Jusid <jonyjusid@gmail.com>
2020-11-30 16:08:28 -0300

    Nuevas actualizaciones

--- Vivero G4/Models/Usuario.cs
+++ Vivero G4/Models/Usuario.cs
@@ -13,22 +13,22 @@ namespace Vivero_G4.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UsuarioId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Debe ingresar un nombre")]
         [RegularExpression(@"^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]{1,40}$",
          ErrorMessage = "Caracteres inválidos.")]
         public string Nombre { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Debe ingresar un apellido")]
         [RegularExpression(@"^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]{1,40}$",
          ErrorMessage = "Caracteres inválidos.")]
         public string Apellido { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Debe ingresar un correo electrónico")]
         [EmailAddress(ErrorMessage = "Email inválido.")]
         public string CorreoElectronico { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Debe ingresar un teléfono")]
         [RegularExpression(@"^[0-9-]{9,15}$",
          ErrorMessage = "Formato 11-2222-3333")]
         public string Telefono { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Debe ingresar una contraseña")]
         [RegularExpression(@"(?!^[0-9]$)(?!^[a-zA-Z]$)^([a-zA-Z0-9]{8,15})$",
          ErrorMessage = "La contraseña debe contener entre 8 y 15 caracteres que contengan letras y números")]
         public string Contraseña { get; set; }
